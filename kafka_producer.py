a0792077057adfa4abd3c3e374b5e7929192d76f
kulwinderjit <thekulwinder@gmail.com>
2020-08-26 08:16:38 -0500

    Some improvements

--- kafka_producer.py
+++ kafka_producer.py
@@ -1,3 +1,4 @@
+from tkinter import messagebox
 from tkinter import *
 from tkinter.ttk import *
 from kafka import KafkaProducer
@@ -8,6 +9,20 @@ import json
 import xmlformatter
 db_name = 'config.db'
 message_timestamp = None
+
+class SqlliteConn(): 
+    def __init__(self, db_name): 
+        self.db_name = db_name 
+        self.connection = None
+  
+    def __enter__(self): 
+        self.connection = sqlite3.connect(self.db_name, detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
+        return self.connection
+  
+    def __exit__(self, exc_type, exc_value, exc_traceback): 
+        self.connection.commit()
+        self.connection.close()
+
 def send_to_kafka(servers, topic, key, value, outtxt):
     try:
         producer = KafkaProducer(bootstrap_servers=servers)
@@ -25,69 +40,62 @@ def send_to_kafka(servers, topic, key, value, outtxt):
         outtxt.see(END)
 
 def init_db():
-    conn = sqlite3.connect(db_name)
-    conn.execute('CREATE TABLE IF NOT EXISTS brokers (value TEXT NOT NULL, timestamp TEXT DEFAULT CURRENT_TIMESTAMP, CONSTRAINT brokers_pk PRIMARY KEY (value));')
-    conn.execute('CREATE TABLE IF NOT EXISTS topics (value TEXT NOT NULL, timestamp TEXT DEFAULT CURRENT_TIMESTAMP, CONSTRAINT topics_pk PRIMARY KEY (value));')
-    conn.execute('CREATE TABLE IF NOT EXISTS keys (value TEXT NOT NULL, timestamp TEXT DEFAULT CURRENT_TIMESTAMP, CONSTRAINT keys_pk PRIMARY KEY (value));')
-    conn.execute('CREATE TABLE IF NOT EXISTS messages (value TEXT NOT NULL, timestamp TEXT DEFAULT CURRENT_TIMESTAMP, CONSTRAINT messages_pk PRIMARY KEY (value));')
-    conn.commit()
-    conn.close()
+    with SqlliteConn(db_name=db_name) as conn:
+        conn.execute('CREATE TABLE IF NOT EXISTS brokers (value TEXT NOT NULL, timestamp TEXT DEFAULT CURRENT_TIMESTAMP, CONSTRAINT brokers_pk PRIMARY KEY (value));')
+        conn.execute('CREATE TABLE IF NOT EXISTS topics (value TEXT NOT NULL, timestamp TEXT DEFAULT CURRENT_TIMESTAMP, CONSTRAINT topics_pk PRIMARY KEY (value));')
+        conn.execute('CREATE TABLE IF NOT EXISTS keys (value TEXT NOT NULL, timestamp TEXT DEFAULT CURRENT_TIMESTAMP, CONSTRAINT keys_pk PRIMARY KEY (value));')
+        conn.execute('CREATE TABLE IF NOT EXISTS messages (value TEXT NOT NULL, timestamp TEXT DEFAULT CURRENT_TIMESTAMP, CONSTRAINT messages_pk PRIMARY KEY (value));')
 
 def get_list(type):
-    conn = sqlite3.connect(db_name)
-    cursor = conn.execute('select value from %s order by timestamp desc' % type)
     l = []
-    for r in cursor:
-        l.append(r[0])
-    conn.close()
+    with SqlliteConn(db_name=db_name) as conn:
+        cursor = conn.execute('select value from %s order by timestamp desc' % type)
+        for r in cursor:
+            l.append(r[0])
     return l
 
 def relenght_tables():
-    conn = sqlite3.connect(db_name)
     maxlen = 20
-    conn.execute('delete from brokers where timestamp in (select b.timestamp from brokers b left join (select timestamp from brokers order by timestamp desc limit ' + str(maxlen) + ') b1 on b1.timestamp = b.timestamp where b1.timestamp is null);')
-    conn.execute('delete from topics where timestamp in (select b.timestamp from topics b left join (select timestamp from topics order by timestamp desc limit ' + str(maxlen) + ') b1 on b1.timestamp = b.timestamp where b1.timestamp is null);')
-    conn.execute('delete from keys where timestamp in (select b.timestamp from keys b left join (select timestamp from keys order by timestamp desc limit ' + str(maxlen) + ') b1 on b1.timestamp = b.timestamp where b1.timestamp is null);')
-    conn.execute('delete from messages where timestamp in (select b.timestamp from messages b left join (select timestamp from messages order by timestamp desc limit ' + str(maxlen) + ') b1 on b1.timestamp = b.timestamp where b1.timestamp is null);')
-    conn.commit()
-    conn.close()
+    with SqlliteConn(db_name=db_name) as conn:
+        conn.execute('delete from brokers where timestamp in (select b.timestamp from brokers b left join (select timestamp from brokers order by timestamp desc limit ' + str(maxlen) + ') b1 on b1.timestamp = b.timestamp where b1.timestamp is null);')
+        conn.execute('delete from topics where timestamp in (select b.timestamp from topics b left join (select timestamp from topics order by timestamp desc limit ' + str(maxlen) + ') b1 on b1.timestamp = b.timestamp where b1.timestamp is null);')
+        conn.execute('delete from keys where timestamp in (select b.timestamp from keys b left join (select timestamp from keys order by timestamp desc limit ' + str(maxlen) + ') b1 on b1.timestamp = b.timestamp where b1.timestamp is null);')
+        conn.execute('delete from messages where timestamp in (select b.timestamp from messages b left join (select timestamp from messages order by timestamp desc limit ' + str(maxlen) + ') b1 on b1.timestamp = b.timestamp where b1.timestamp is null);')
 
 def send():
     _broker = str(servers.get()).strip()
     _topic = str(topic.get()).strip()
     _key = str(key.get()).strip()
     _value = str(value.get('1.0', 'end-1c')).strip()
-    conn = sqlite3.connect('config.db')
-    conn.execute('insert or replace into brokers(value) values(?)', (_broker,))
-    conn.execute('insert or replace into topics(value) values(?)', (_topic,))
-    conn.execute('insert or replace into keys(value) values(?)', (_key,))
-    conn.execute('insert or replace into messages(value) values(?)', (_value,))
-    conn.commit()
-    conn.close()
+    with SqlliteConn(db_name=db_name) as conn:
+        conn.execute('insert or replace into brokers(value) values(?)', (_broker,))
+        conn.execute('insert or replace into topics(value) values(?)', (_topic,))
+        conn.execute('insert or replace into keys(value) values(?)', (_key,))
+        conn.execute('insert or replace into messages(value) values(?)', (_value,))
     relenght_tables()
     update_lists()
     if len(_broker)>0 and len(_topic)>0 and len(_key)>0 and len(_value)>0:
         send_to_kafka(_broker, _topic, _key, _value, outtxt)
 
 def get_latest_msg():
-    conn = sqlite3.connect(db_name)
-    cursor = conn.execute('select value,timestamp from messages order by timestamp desc limit 1')
-    for r in cursor:
-        return (r[0], r[1])
+    with SqlliteConn(db_name=db_name) as conn:
+        cursor = conn.execute('select value,timestamp from messages order by timestamp desc limit 1')
+        for r in cursor:
+            return (r[0], r[1])
     return (None,None)
 
 def get_prev_msg(timestamp):
-    conn = sqlite3.connect(db_name)
-    cursor = conn.execute('select value,timestamp from messages where timestamp < ? order by timestamp desc limit 1', (timestamp,))
-    for r in cursor:
-        return (r[0], r[1])
+    with SqlliteConn(db_name=db_name) as conn:
+        cursor = conn.execute('select value,timestamp from messages where timestamp < ? order by timestamp desc limit 1', (timestamp,))
+        for r in cursor:
+            return (r[0], r[1])
     return (None,None)
 
 def get_next_msg(timestamp):
-    conn = sqlite3.connect(db_name)
-    cursor = conn.execute('select value,timestamp from messages where timestamp > ? order by timestamp asc limit 1', (timestamp,))
-    for r in cursor:
-        return (r[0], r[1])
+    with SqlliteConn(db_name=db_name) as conn:
+        cursor = conn.execute('select value,timestamp from messages where timestamp > ? order by timestamp asc limit 1', (timestamp,))
+        for r in cursor:
+            return (r[0], r[1])
     return (None,None)
 
 def update_lists():
@@ -128,10 +136,15 @@ def format_json():
         outtxt.insert(END, str(datetime.now()) + ': ' + str('Unable to format as json') + '\n')
         outtxt.see(END)
 def format_xml():
-    _value = str(value.get('1.0', 'end-1c')).strip()
-    formatter = xmlformatter.Formatter(indent="1", indent_char="\t", encoding_output="UTF-8", preserve=["literal"])
-    value.delete('1.0', END)
-    value.insert(END, formatter.format_string(_value))
+    try:
+        _value = str(value.get('1.0', 'end-1c')).strip()
+        formatter = xmlformatter.Formatter(indent="1", indent_char="\t", encoding_output="UTF-8", preserve=["literal"])
+        _value = formatter.format_string(_value)
+        if _value:
+            value.delete('1.0', END)
+            value.insert(END, _value)
+    except:
+        None
 def set_next_msg():
     global message_timestamp
     msg,ts = get_next_msg(message_timestamp)
@@ -139,11 +152,14 @@ def set_next_msg():
         message_timestamp = ts
         value.delete('1.0', END)
         value.insert(END, msg)
+def on_closing():
+    if messagebox.askokcancel("Quit", "Do you want to quit?"):
+        main_window.destroy()
 
 init_db()
 main_window = Tk()
 main_window.title('Kafka Producer by Kulwinderjit')
-
+main_window.protocol("WM_DELETE_WINDOW", on_closing)
 buttons_frame = Frame(main_window)
 servers_label = LabelFrame(buttons_frame, text='Bootstrap servers', padding='2 2 2 2')
 servers = Combobox(servers_label)
@@ -181,7 +197,6 @@ value.grid(row=0, column=0, padx=2, pady=2, sticky=N+W+E)
 value_label.grid(row=1, column=0, padx=5, sticky=N+W+E)
 value_label.columnconfigure(0, weight=1)
 value_label.rowconfigure(0, weight=1)
-#value_label.rowconfigure(1, weight=1)
 scrollb.grid(row=0, column=1, sticky=N+S)
 scrollb_h.grid(row=1, column=0, sticky=E+W)
 next_value = Button(value, text='>', padding='0 0 0 0', width=4, command=set_next_msg, cursor='arrow')
